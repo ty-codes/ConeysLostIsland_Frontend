7f59f3a71c2afba41cdcdee8c77e9c288fdb2f98
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-01-15 03:34:56 -0800

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,21 @@
+class Auth {
+     // setup the class and hide the body by default
+    constructor() {
+        document.querySelector("body").style.display = "none";
+        const auth = localStorage.getItem("token");
+        this.validateAuth(auth);
+    }
+    // check to see if the localStorage item passed to the function is valid and set
+    validateAuth(auth) {
+        if (auth != "123") {
+            window.location.replace("login.html");
+        } else {
+            document.querySelector("body").style.display = "block";
+        }
+    }
+    // will remove the localStorage item and redirect to login  screen
+    logOut() {
+        localStorage.removeItem("token");
+        window.location.replace("login.html");
+    }
+}
--- index.js
+++ index.js
@@ -1,67 +1,121 @@
 var items = [];
 
-const ErrorText = 
-    `
+const ErrorText =
+  `
       <div class="error_wrap">
         <p>Failed to fetch<p>
         <button style="margin: 0;" class="error_btn btn btn-danger btn_danger" onclick="window.location.reload()" > Try again</button>
       </div>
     `
 
-const  getItems =  async () => {  
+const getItems = async () => {
   try {
-    const resp = await fetch('https://coneyslostisland.onrender.com/home')
+    const resp = await fetch('http://127.0.0.1:8000/home')
     const data = await resp.json()
     return data;
-  } catch(err) {
+  } catch (err) {
     document.getElementById('itemlist').innerHTML = ErrorText;
   }
-  
+
 }
 getItems().then(data => {
   data;
-  items.push(...data)
+  data && items.push(...data)
   localStorage.setItem('items', JSON.stringify(data))
   storage = JSON.parse(localStorage.getItem('items'));
   data.length > 0 && runData(data)
 })
 
-
-// search box
-function registerInputChange(){
-  var x = document.getElementById('search_btn').value.toLowerCase();
-  var data = items;
-  // console.log(data)
-  var newItemList = [];
-data.map((dt,key)=> {
-  var query = dt.description.toLowerCase()
-  var query2 = dt.name.toLowerCase()
-  if(query.includes(`${x}`)===true || query2.includes(`${x}`) === true ){
-    newItemList.push(dt)
-  } 
-  runData(newItemList)
-})
+var item = {
+  name: null,
+  location:  null,
+  date_lost: null,
+  description: null,
+  ticket: null
 }
 
-function runData (myList) {
-  var result = myList?.map((dt,key)=> {
-         return (
-          `<div class="col-md-4 " key=${dt._id}>
+const claimItem = async () => {
+  document.getElementById("claimItem").classList.remove("show");
+  var date_lost = document.getElementById('date_lost');
+  var name = document.getElementById('name');
+  var location = document.getElementById('location');
+  var description = document.getElementById('description');
+  var ticket = document.getElementById('ticket');
+  // assign values to properties
+  nItem = Object.create(item)
+  
+  nItem.name = name.value
+  nItem.description = description.value;
+  nItem.date_lost = date_lost.value
+  nItem.location = location.value;
+  nItem.ticket = ticket.value;
+
+  try {
+  await fetch('http://127.0.0.1:8000/claim', {
+    method: "POST",
+    body: JSON.stringify(nItem),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }).then(async (res) => {
+    if (res.status === 200) {
+      const data = await res.json();
+      data &&
+      Swal.fire({
+        title: 'Success!',
+        html: 
+        `Your claim has been documented. This is your token: ${data.claimToken}. Please copy the token and present at the help desk.`,
+        icon: 'success'
+      }
+      )
+    }
+  })
+  
+  } catch (err) {
+    console.log('ERROR', err);
+    Swal.fire(
+      'Error!',
+      'Could not document item. Try again.',
+      'error'
+    )
+  }
+}
+
+  // search box
+  function registerInputChange() {
+    var x = document.getElementById('search_btn').value.toLowerCase();
+    var data = items;
+    // console.log(data)
+    var newItemList = [];
+    data.map((dt, key) => {
+      var query = dt.description.toLowerCase()
+      var query2 = dt.name.toLowerCase()
+      if (query.includes(`${x}`) === true || query2.includes(`${x}`) === true) {
+        newItemList.push(dt)
+      }
+      runData(newItemList)
+    })
+  }
+
+  function runData(myList) {
+    var result = myList?.map((dt, key) => {
+      return (
+        `<div class="col-md-4 " key=${dt._id}>
           <div class="card item">
             <img src="${dt.image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title car_model" id='card_model'> ${dt.name}</h5>
               <p id="card-text" class="car_desc">
               ${dt.description} </p>
-              
+              <button id="claim" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#claimItem">Claim</button>
             </div>
             
           </div>
         </div>`
-         )
-    
-      
-      
-  })
-  document.getElementById('itemlist').innerHTML = result && result;
-}
+      )
+
+
+
+    })
+    document.getElementById('itemlist').innerHTML = result && result;
+  }
